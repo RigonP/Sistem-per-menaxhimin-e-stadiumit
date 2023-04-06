bba21804b421b398490a1e2ffeedf1f83d0a389e
RigonP <rp57421@ubt-uni.net>
2023-04-06 22:48:17 +0200

    Krijim i pjeses se main

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -3,7 +3,18 @@ import './main.css'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <section className='main container section'>
+
+      <div className="secTitle">
+        <h3 className="title">
+          Most visited destinations
+        </h3>
+      </div>
+
+      <div className="secContent grid">
+        
+      </div>
+    </section>
   )
 }
 
