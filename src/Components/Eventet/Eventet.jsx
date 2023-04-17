0a1b7ed3ccf84aee6484fcc5016ec31c0d2b82c0
RigonP <rp57421@ubt-uni.net>
2023-04-17 10:32:22 +0200

    Shtimi i Navbar tek Eventet

--- src/Components/Eventet/Eventet.jsx
+++ src/Components/Eventet/Eventet.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import './Eventet.css'
 import Footer from '../Footer/Footer'
-
+import Navbar from '../Navbar/Navbar'
 const Eventet = () => {
   return (
     <>
+    <Navbar />
       <div className="events">
         <h2>EVENTS</h2>
       </div>
-      <Footer />
+    <Footer />
     </>
   )
 }
