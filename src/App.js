dddafba0e687a181bb167a1af6c08a7571c6eaf6
Ariolaa <117186596+Ariolaa@users.noreply.github.com>
2023-04-17 04:04:03 +0200

    Importimi ContactForm ne App

--- src/App.js
+++ src/App.js
@@ -6,22 +6,26 @@ import Main from './Components/Main/Main';
 import Footer from './Components/Footer/Footer';
 import Programs from './Components/Programs/Programs';
 import Homeslider from './Components/Slider/homeslider/slider';
-// import Eventet from './Components/Eventet/Eventet'
+// import Eventet from './Components/Eventet/Eventet' //
+import Form from './Components/Contact/Form';  
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 const App = () => {
   return (
     <Router>
-	    <Navbar />
-	    {/* <Routes>
-		    <Route path='/eventet' element={<Eventet/>} />
-	    </Routes> */}
-      <Homeslider />
-      <Main />
-      <Home />
-      <Programs />
-      <Footer />
-	  </Router>
+    <Routes>
+      <Route path="/contact" element={<Form />} />
+    </Routes>
+    <Navbar />
+    {/* <Routes>
+      <Route path='/eventet' element={<Eventet/>} />
+    </Routes> */}
+    <Homeslider />
+    <Main />
+    <Home />
+    <Programs />
+    <Footer />
+  </Router>
   )
 }
 
