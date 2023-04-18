9c2b6d1086af4a8723a9e7a0c480916c8448139d
Suadi49 <suadsylejmani49@gmail.com>
2023-04-18 04:49:55 +0200

    Startimi i Shop page me React

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css'
 import Eventet from './Components/Eventet/Eventet' //
 import Form from './Components/Contact/Form';  
+import Shop from './Components/Shop/Shop'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import HomePage from './Components/HomePage/HomePage';
 
@@ -12,6 +13,7 @@ const App = () => {
       <Route path='/' element={<HomePage />} />
       <Route path='/eventet' element={<Eventet />} />
       <Route path="/contact" element={<Form />} />
+      <Route path="/shop" element={<Shop />} />
     </Routes>
   </Router>
   )
