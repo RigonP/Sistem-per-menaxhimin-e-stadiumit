512a831b7bdd4286201aa0d2581b9bf2137dfeda
RigonP <rp57421@ubt-uni.net>
2023-04-19 23:09:01 +0200

    Komentimi i rreshtave te panevojshem tek HomePage

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import '../../App.css'
 import Navbar from '../Navbar/Navbar'
-import Eventet from '../Eventet/Eventet'
+// import Eventet from '../Eventet/Eventet'
 import Homeslider from '../Slider/homeslider/slider'
 // import Main from '../Main/Main'
 import Home from '../Home/Home'
 import Programs from '../Programs/Programs'
 import Footer from '../Footer/Footer'
-import {Routes, Route} from 'react-router-dom';
+// import {Routes, Route} from 'react-router-dom';
 import Testimonials from '../Testimonials/Testimonials'
 const HomePage = () => {
   return (
     <>
     <Navbar />
-    <Routes>
+    {/* <Routes>
       <Route path='/eventet' element={<Eventet/>} />
-    </Routes>
+    </Routes> */}
     <Homeslider />
     <Testimonials />
     <Home />
