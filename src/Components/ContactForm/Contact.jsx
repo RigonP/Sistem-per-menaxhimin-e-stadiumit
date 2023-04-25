fa5cc6f9712f626e50b41cee9ec45858ff35ea66
RigonP <rp57421@ubt-uni.net>
2023-04-25 22:37:22 +0200

    Shtimi i pjeses se hartes tek contact form

--- src/Components/ContactForm/Contact.jsx
+++ src/Components/ContactForm/Contact.jsx
@@ -79,7 +79,17 @@ const Contact = () => {
               </ul>
             </div>
           </div>
-          
+          {/* Map section */}
+          <div className="contact-map">
+            <iframe title="Google Maps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2933.928454076262!2d21.1544899753821!3d42.662871515918106!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13549ee398234689%3A0x1ecdd21bb52cbd0f!2zU3RhZGl1bSAiIPCdkIXwnZCA8J2Qg_CdkIjwnZCLIPCdkJXwnZCO8J2QivCdkJHwnZCR8J2QiCAiIFByaXN0aW5h!5e0!3m2!1sen!2s!4v1682011069333!5m2!1sen!2s"
+                width="600"
+                height="450"
+                style={{border:"0" }}
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade">
+            </iframe>
+          </div>
           
         </div>
         </div>
