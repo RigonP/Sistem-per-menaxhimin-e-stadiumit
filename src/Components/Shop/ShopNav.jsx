6b81ef517b4e347214131be1783eccea5ae8175a
Suadi49 <suadsylejmani49@gmail.com>
2023-04-27 01:15:57 +0200

    Rregullimi i Navbarit ne Shop page

---
+++ src/Components/Shop/ShopNav.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { BiFontSize } from 'react-icons/bi';
+import {MdOutlineStadium} from 'react-icons/md';
+import { Link } from "react-router-dom";
+import './shop.css'
+
+
+function MyNavbar() {
+  return (
+    <Navbar className='container' expand="md">
+            <div className="logoDiv">
+                <Link to="/" className="logo flex" >
+                    <h1 className="logo-text"><MdOutlineStadium className="icon"/> Kosovo Stadium.</h1>
+                </Link>
+            </div>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" data-target="#basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link style={{color: 'black'}} href="#" text-dark>Kits</Nav.Link>
+          <Nav.Link href="#" style={{color: 'black'}}>Shop By Player</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default MyNavbar;
