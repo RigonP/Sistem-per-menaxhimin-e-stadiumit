8360ca305e9ec3288a29a2ec101e34e46642b815
RigonP <rp57421@ubt-uni.net>
2023-04-26 18:12:55 +0200

    Shtimi i footer tek contact, shtimi i animacioneve

--- src/Components/ContactForm/Contact.jsx
+++ src/Components/ContactForm/Contact.jsx
@@ -1,7 +1,8 @@
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import './Contact.css';
 import Navbar from '../Navbar/Navbar'
+import Footer from '../Footer/Footer'
 import {GrMapLocation} from 'react-icons/gr'
 import {HiOutlineMailOpen} from 'react-icons/hi'
 import {BsFillTelephoneFill} from 'react-icons/bs'
@@ -11,17 +12,23 @@ import {FiInstagram} from 'react-icons/fi'
 
 import { validateName, validateSurname, validateEmail, validatePhone, validateMessage, validateForm } from './script';
 
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 
 const Contact = () => {
+  useEffect(() => {
+    Aos.init({duration: 2000, easing: 'ease-out-back'})
+  },[])
+
     return (
         <>
         <Navbar/>
         <div class="events">
-          <h2>KONTAKT</h2>
+          <h2 data-aos='fade-right'>KONTAKT</h2>
         </div>
         <div className="contactUs">
           <div className="box">
-            <div className="contact form">
+            <div className="contact form" data-aos="flip-up">
               <h3>Shkruani mesazh</h3>
               <form>
                 <div className="formBox">
@@ -61,7 +68,7 @@ const Contact = () => {
                 </div>
               </form>
             </div>
-            <div className="contact info">
+            <div className="contact info" data-aos="flip-down">
               <h3>Na kontaktoni</h3>
               <div className="infoBox">
                 <div>
@@ -84,7 +91,7 @@ const Contact = () => {
             </div>
           </div>
           {/* Map section */}
-          <div className="contact-map">
+          <div className="contact-map" data-aos="flip-up">
             <iframe title="Google Maps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2933.928454076262!2d21.1544899753821!3d42.662871515918106!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13549ee398234689%3A0x1ecdd21bb52cbd0f!2zU3RhZGl1bSAiIPCdkIXwnZCA8J2Qg_CdkIjwnZCLIPCdkJXwnZCO8J2QivCdkJHwnZCR8J2QiCAiIFByaXN0aW5h!5e0!3m2!1sen!2s!4v1682011069333!5m2!1sen!2s"
                 width="600"
                 height="450"
@@ -97,6 +104,7 @@ const Contact = () => {
           
         </div>
         </div>
+        <Footer />
         </>
       );
 }
