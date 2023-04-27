a8836182ad1d93d14cf0d0f958b85267a31b4a01
artindervishii <ad58448@ubt-uni.net>
2023-04-27 03:59:39 +0200

    Rregullimi i menaxhimit te produkteve , delete, add, update prej adminit

---
+++ src/main/java/com/inn/stadium/repository/ProductDao.java
@@ -0,0 +1,23 @@
+package com.inn.stadium.repository;
+
+import com.inn.stadium.POJO.Product;
+import com.inn.stadium.wrapper.ProductWrapper;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+public interface ProductDao extends JpaRepository<Product,Integer> {
+
+    List<ProductWrapper> getAllProduct();
+
+    @Modifying
+    @Transactional
+    Integer updateProductStatus(@Param("status") String status,@Param("id") Integer id);
+
+    List<ProductWrapper>getProductByCategory(@Param("id") Integer id);
+
+    ProductWrapper getProductById(@Param("id") Integer id);
+}
