b3c084c1a728b5731fbf016b6c0b46fb864539df
RigonP <rp57421@ubt-uni.net>
2023-04-28 17:02:01 +0200

    Shtimi i animacioneve shtese te titulli i eventeve

--- src/Components/Eventet/Eventet.jsx
+++ src/Components/Eventet/Eventet.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import './Eventet.css'
 import Footer from '../Footer/Footer'
 import Navbar from '../Navbar/Navbar'
 import Main from '../Main/Main'
 import Sponsors from './Sponsors/Sponsors'
 
+
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
 const Eventet = () => {
+  useEffect(() => {
+    Aos.init({duration: 2000})
+  },[])
+
   return (
     <>
     <Navbar />
       <div className="events">
-        <h2>EVENTET</h2>
+        <h2 data-aos='fade-right'>EVENTET</h2>
       </div>
     <Main />
     <Sponsors />
