606c73b4640538f4ea69224b5f567c2b1488bffc
RigonP <rp57421@ubt-uni.net>
2023-04-28 15:39:21 +0200

    Ripercaktim i dependencies, krijimi i sponsors

---
+++ src/Components/Eventet/Sponsors/Sponsors.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+
+const Sponsors = () => {
+  return (
+    <div className="sponsors-container">
+      <h2>Our Sponsors</h2>
+      <div className="sponsors-list">
+        <div className="sponsor-item">
+          <img src={Foto1} alt="Sponsor 1" />
+        </div>
+        <div className="sponsor-item">
+          <img src={Foto1} alt="Sponsor 2" />
+        </div>
+        <div className="sponsor-item">
+          <img src={Foto1} alt="Sponsor 3" />
+        </div>
+        <div className="sponsor-item">
+          <img src={Foto1} alt="Sponsor 4" />
+        </div>
+        <div className="sponsor-item">
+          <img src={Foto1} alt="Sponsor 5" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sponsors;
