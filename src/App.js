7cae692b3110a8c837c4f4d410c42f7397da30dc
Ariolaa <117186596+Ariolaa@users.noreply.github.com>
2023-04-29 23:51:38 +0200

    Routepath Tours ne App js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Eventet from './Components/Eventet/Eventet'
 import Contact from './Components/ContactForm/Contact';  
 import Fans from './Components/Fans/Fans'
 import Shop from './Components/Shop/Shop'
+import Tours from './Components/Tours/Tours'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import HomePage from './Components/HomePage/HomePage';
 
@@ -16,6 +17,7 @@ const App = () => {
       <Route path="/contact" element={<Contact />} />
       <Route path="/shop" element={<Shop />} />
       <Route path='/fans' element={<Fans />} /> 
+      <Route path='/tours' element={<Tours />} />
     </Routes>
   </Router>
   )
