b0d9cb308e79ace62b30883d24ee064161a4ec0c
RigonP <rp57421@ubt-uni.net>
2023-04-30 23:03:59 +0200

    Shtimi i navbar-it tek TIKETAT

--- src/Components/Tiketat/Tiketat.jsx
+++ src/Components/Tiketat/Tiketat.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Tiketat.css'
+import Navbar from '../Navbar/Navbar'
 
 
 const Tiketat = () => {
   return (
     <>
+    <Navbar />
       <div className="events">
         <h2>TIKETAT</h2>
       </div>
