772c736249be1403761b49ad7c24fb977448b9b6
artindervishii <ad58448@ubt-uni.net>
2023-05-02 03:38:56 +0200

    bill / deleteByID bill API

--- src/main/java/com/inn/stadium/rest/BillRest.java
+++ src/main/java/com/inn/stadium/rest/BillRest.java
@@ -9,21 +9,14 @@ import java.util.Map;
 
 @RequestMapping(path = "/bill")
 public interface BillRest {
-
     @PostMapping(path = "/generateReport")
     public ResponseEntity<String>generateReport(@RequestBody Map<String,Object>requestMap);
-
-
     @GetMapping(path = "/getBills")
     public ResponseEntity<List<Bill>>getBills();
-
     @PostMapping(path = "/getPdf")
     public  ResponseEntity<byte[]> getPdf(@RequestBody Map<String,Object> requestMap);
-
     @PostMapping(path = "/delete/{id}")
     ResponseEntity<String>deleteBill(@PathVariable Integer id);
-
-
 }
 
 
