8ace71ee8508e1966f8e93aa48d0c0f7e1ffdfe7
artindervishii <ad58448@ubt-uni.net>
2023-05-02 03:36:43 +0200

    category / update API

--- src/main/java/com/inn/stadium/rest/CategoryRest.java
+++ src/main/java/com/inn/stadium/rest/CategoryRest.java
@@ -10,10 +10,8 @@ import java.util.Map;
 
 @RequestMapping(path = "/category")
 public interface CategoryRest {
-
     @PostMapping (path="/add")
     public ResponseEntity<String > addNewCategory(@RequestBody(required = true) Map<String,String>requestMap);
-
 
     @GetMapping(path = "/get")
     public ResponseEntity<List<Category>> getAllCategory(@RequestParam(required = false) String filterValue);
