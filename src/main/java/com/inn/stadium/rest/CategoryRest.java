b94bac950de514a7a9bddbf2f21cec41803fa55f
artindervishii <ad58448@ubt-uni.net>
2023-05-02 03:36:26 +0200

    category / getALLCategory API

--- src/main/java/com/inn/stadium/rest/CategoryRest.java
+++ src/main/java/com/inn/stadium/rest/CategoryRest.java
@@ -11,9 +11,9 @@ import java.util.Map;
 @RequestMapping(path = "/category")
 public interface CategoryRest {
 
-
     @PostMapping (path="/add")
     public ResponseEntity<String > addNewCategory(@RequestBody(required = true) Map<String,String>requestMap);
+
 
     @GetMapping(path = "/get")
     public ResponseEntity<List<Category>> getAllCategory(@RequestParam(required = false) String filterValue);
