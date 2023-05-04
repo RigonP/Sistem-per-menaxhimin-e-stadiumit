d1a7474c921e9a66ab856ab1f7e6e4fae979bb9e
RigonP <rp57421@ubt-uni.net>
2023-05-05 00:38:25 +0200

    Krijimi i dropdown me dy opsione tek TIKETAT

--- src/Components/Tiketat/Tiketat.jsx
+++ src/Components/Tiketat/Tiketat.jsx
@@ -1,9 +1,14 @@
-import React, {useEffect} from 'react'
+import React, {useState, useEffect} from 'react'
 import './Tiketat.css'
 import Navbar from '../Navbar/Navbar'
 
 import Aos from 'aos'
 import 'aos/dist/aos.css'
+
+const eventTypes = [
+  { label: 'Futboll', value: 'Futboll' },
+  { label: 'Koncert', value: 'Koncert' }
+];
 
 const Tiketat = () => {
 
@@ -11,12 +16,30 @@ const Tiketat = () => {
     Aos.init({duration: 2000})
   },[])
 
+  const [selectedType, setSelectedType] = useState('');
+
+  function handleTypeChange(event) {
+    setSelectedType(event.target.value);
+  }
+
   return (
     <>
     <Navbar />
       <div className="events">
         <h2 data-aos='fade-right'>TIKETAT</h2>
       </div>
+
+    <div className="dropdownContainer">
+    <div className='titleE'>
+      <h2>Zgjedhni llojin e eventit</h2>
+    </div>
+    
+    <div className="dropdown">
+      <select className='selectT' value={selectedType} onChange={handleTypeChange}>
+        <option value="">Zgjedhni llojin e eventit:</option>
+      </select>
+    </div>
+    </div>
     </>
   )
 }
