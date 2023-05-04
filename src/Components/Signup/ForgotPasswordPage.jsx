79d472b6cf0db0f6cfe3f55ebcef2da310478394
artindervishii <ad58448@ubt-uni.net>
2023-05-04 20:39:22 +0200

    app properties change

--- src/Components/Signup/ForgotPasswordPage.jsx
+++ src/Components/Signup/ForgotPasswordPage.jsx
@@ -1,54 +1,49 @@
-// import React, { useState } from "react";
-// import axios from "axios";
+import React, { useState } from "react";
+import axios from "axios";
 
-// import './changepass.css';
-//
-// function ForgotPasswordPage() {
-//     const [email, setEmail] = useState("");
-//     const [errorMessage, setErrorMessage] = useState("");
-//     const [successMessage, setSuccessMessage] = useState("");
+function ForgotPasswordPage() {
+    const [email, setEmail] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+    const [successMessage, setSuccessMessage] = useState("");
 
-//
-//     const handleSubmit = (e) => {
-//         e.preventDefault();
-//         axios
-//             .post("http://localhost:8080/user/forgotPassword",
-//                 { email }, { withCredentials: true })
-//             .then((res) => {
-//                 if (res.status === 200) {
-//                     setSuccessMessage(
-//                         "Shikoni ju lutem ne emailin tuaj, per te shikuar passwordin"
-//                     );
-//                 }
-//             })
-//             .catch((err) => {
-//                 setErrorMessage(
-//                     "Dicka shkoi gabim. Ju lutem provoni përsëri ose regjistrohuni."
-//                 );
-//             });
-//     };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios
+            .post("http://localhost:8080/user/forgotPassword",
+                { email }, { withCredentials: true })
+            .then((res) => {
+                if (res.status === 200) {
+                    setSuccessMessage(res.data);
+                    setErrorMessage("");
+                }
+            })
+            .catch((err) => {
+                setErrorMessage(
+                    "Dicka shkoi gabim. Ju lutem provoni përsëri ose regjistrohuni."
+                );
+                setSuccessMessage("");
+            });
+    };
 
+    return (
+        <div>
+            <h1>Keni harruar fjalëkalimin?</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="email">Email:</label>
+                <input
+                    type="email"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+                <br />
+                <button type="submit">Dërgo</button>
+            </form>
+            {errorMessage && <p>{errorMessage}</p>}
+            {successMessage && <p>{successMessage}</p>}
+        </div>
+    );
+}
 
-//
-//
-//     return (
-//         <div>
-//             <h1>Keni harruar fjalëkalimin?</h1>
-//             <form onSubmit={handleSubmit}>
-//                 <label htmlFor="email">Email:</label>
-//                 <input
-//                     type="email"
-//                     id="email"
-//                     value={email}
-//                     onChange={(e) => setEmail(e.target.value)}
-//                     required
-//                 />
-//                 <br />
-//                 <button type="submit">Dërgo</button>
-//             </form>
-//             {errorMessage && <p>{errorMessage}</p>}
-//             {successMessage && <p>{successMessage}</p>}
-//         </div>
-//     );
-// }
-// export default ForgotPasswordPage
+export default ForgotPasswordPage;
