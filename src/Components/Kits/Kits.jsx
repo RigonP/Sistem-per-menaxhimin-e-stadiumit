bcf2f3503bc3bd69d7e12d8ad89a7c9ca892b093
Suadi49 <suadsylejmani49@gmail.com>
2023-05-05 17:59:29 +0200

    Disa ndryshime

--- src/Components/Kits/Kits.jsx
+++ src/Components/Kits/Kits.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import './kits.css'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
-
 import MyNavbar from '../Shop/ShopN'
 
 import img1 from '../../images/collection_banner__mens_kits_1070x.webp'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 
@@ -42,6 +41,63 @@ function Kits(){
                     <a href='#' className='read-link'>READ MORE</a>
                 </div>
             </div>
+
+            
+            <div className="container dropdown-container">
+                <div className="row g-1">
+                    <div className="col-md-1">
+                        <div className="dropdown">
+                            <button style={{color:"black"}} className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                KITS | SIZE
+                            </button>
+                            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                <li><a className="dropdown-item" href="#">Item 1</a></li>
+                                <li><a className="dropdown-item" href="#">Item 2</a></li>
+                                <li><a className="dropdown-item" href="#">Item 3</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col-md-1">
+                        <div className="dropdown">
+                            <button style={{color:"black"}} className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+                                COLORS
+                            </button>
+                            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton2">
+                                <li><a className="dropdown-item" href="#">Item 1</a></li>
+                                <li><a className="dropdown-item" href="#">Item 2</a></li>
+                                <li><a className="dropdown-item" href="#">Item 3</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col-md-1">
+                    <div className="dropdown">
+                            <button style={{color:"black"}} className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton3" data-bs-toggle="dropdown" aria-expanded="false">
+                                MATCHWEAR
+                            </button>
+                            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton3">
+                                <li><a className="dropdown-item" href="#">Item 1</a></li>
+                                <li><a className="dropdown-item" href="#">Item 2</a></li>
+                                <li><a className="dropdown-item" href="#">Item 3</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col-md-1">
+                        <div className="dropdown">
+                            <button style={{color:"black"}} className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton4" data-bs-toggle="dropdown" aria-expanded="false">
+                                SORT BY
+                            </button>
+                            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton4">
+                                <li><a className="dropdown-item" href="#">Item 1</a></li>
+                                <li><a className="dropdown-item" href="#">Item 2</a></li>
+                                <li><a className="dropdown-item" href="#">Item 3</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                    
+            
+
 
 
 
