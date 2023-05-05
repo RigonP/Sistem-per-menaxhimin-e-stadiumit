0b29463fc0b2c47d401f652e49636aa570949472
RigonP <rp57421@ubt-uni.net>
2023-05-05 17:01:37 +0200

    Shtimi i çmimit per ulese tek stadiumi per koncert

--- src/Components/Tiketat/ConcertSeat.jsx
+++ src/Components/Tiketat/ConcertSeat.jsx
@@ -7,6 +7,18 @@ const ConcertSeat = () => {
     <SeatsioSeatingChart
     workspaceKey="0f6f6071-f800-42a4-9105-66d8abf35eee"
     event="6a7b5888-52d3-4e01-94f0-1facfcd87b3b"
+    pricing={[
+      {'category': 'Para', 'price': 30},
+      {'category': 'Para-Kati2', 'price': 35},
+      {'category': 'VIP', 'price': 80},
+      {'category': 'D-Kati-1', 'price': 33},
+      {'category': 'D-Kati-2', 'price': 40},
+      {'category': 'D-Kati-2', 'price': 45},
+      {'category': 'Disabilities', 'price': 40},
+      {'category': 'M-Para', 'price': 48},
+      {'category': 'M-2', 'price': 45},
+    ]}
+    priceFormatter={price => '€' + price}
     />
   </div>
   )
