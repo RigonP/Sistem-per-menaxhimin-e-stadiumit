b8f702cde18063fc7822070610f41076bb5eb6be
RigonP <rp57421@ubt-uni.net>
2023-05-05 16:56:28 +0200

    Shtim stilizimi tek dy pamjet vizuale

--- src/Components/Tiketat/ConcertSeat.jsx
+++ src/Components/Tiketat/ConcertSeat.jsx
@@ -3,7 +3,7 @@ import { SeatsioSeatingChart } from '@seatsio/seatsio-react';
 
 const ConcertSeat = () => {
   return( 
-  <div>
+  <div style={{ 'height': '600px' }}>
     <SeatsioSeatingChart
     workspaceKey="0f6f6071-f800-42a4-9105-66d8abf35eee"
     event="6a7b5888-52d3-4e01-94f0-1facfcd87b3b"
--- src/Components/Tiketat/StadiumSeat.jsx
+++ src/Components/Tiketat/StadiumSeat.jsx
@@ -3,7 +3,7 @@ import { SeatsioSeatingChart } from '@seatsio/seatsio-react';
 
 const StadiumSeat = () => {
   return(
-      <div>
+      <div  style={{ 'height': '600px' }}>
         <SeatsioSeatingChart
           workspaceKey="0f6f6071-f800-42a4-9105-66d8abf35eee"
           event="4748dd74-954c-4a21-96a0-8f1b3cc0a3a7"
