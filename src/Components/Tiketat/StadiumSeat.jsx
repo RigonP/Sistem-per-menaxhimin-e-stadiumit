048dabfb1e5bb59ee69d0be3f1e9c01874e20645
RigonP <rp57421@ubt-uni.net>
2023-05-05 11:08:06 +0200

    Rikthimi i eventit per pamje realiste

--- src/Components/Tiketat/StadiumSeat.jsx
+++ src/Components/Tiketat/StadiumSeat.jsx
@@ -7,6 +7,7 @@ const StadiumSeat = () => {
       <div>
         <SeatsioSeatingChart
           workspaceKey="0f6f6071-f800-42a4-9105-66d8abf35eee"
+          event="4748dd74-954c-4a21-96a0-8f1b3cc0a3a7"
           pricing={[
             {'category': 'Jug', 'price': 25},
             {'category': 'Veriu', 'price': 25}
