630fb03f3de1b08237228f2648d1f3deb97c743d
RigonP <rp57421@ubt-uni.net>
2023-05-05 10:54:01 +0200

    Importimi i SeatsioSeatingChart dhe thirrja e tij

--- src/Components/Tiketat/StadiumSeat.jsx
+++ src/Components/Tiketat/StadiumSeat.jsx
@@ -1,11 +1,15 @@
 
 import React from 'react';
+import { SeatsioSeatingChart } from '@seatsio/seatsio-react';
 
 const StadiumSeat = () => {
   return(
-    <div>
-
-    </div>
+      <div>
+        <SeatsioSeatingChart
+          workspaceKey="0f6f6071-f800-42a4-9105-66d8abf35eee"
+          event="4748dd74-954c-4a21-96a0-8f1b3cc0a3a7"
+        />
+      </div>
   )
 };
 
