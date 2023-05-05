5a40ac67125e0675a4d0e78c63f69f45dbbfd80e
RigonP <rp57421@ubt-uni.net>
2023-05-05 12:11:39 +0200

    Shtimi i çmimit per hendikep dhe jug-perendim

--- src/Components/Tiketat/StadiumSeat.jsx
+++ src/Components/Tiketat/StadiumSeat.jsx
@@ -10,7 +10,9 @@ const StadiumSeat = () => {
           event="4748dd74-954c-4a21-96a0-8f1b3cc0a3a7"
           pricing={[
             {'category': 'Jug', 'price': 25},
-            {'category': 'Veriu', 'price': 25}
+            {'category': 'Veriu', 'price': 25},
+            {'category': 'Hendikep', 'price': 35},
+            {'category': 'JugPerendim', 'price': 38}
           ]}
         />
       </div>
