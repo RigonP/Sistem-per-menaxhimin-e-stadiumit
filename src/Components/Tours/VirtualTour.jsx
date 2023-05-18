2835a8285cb7b86e99abf3fa26052afb8b58f11b
RigonP <rp57421@ubt-uni.net>
2023-05-19 00:18:42 +0200

    Thirrja e Footer tek VirtualTour

--- src/Components/Tours/VirtualTour.jsx
+++ src/Components/Tours/VirtualTour.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./VirtualTour.css"
 import Navbar from '../Navbar/Navbar'
+import Footer from '../Footer/Footer'
 
 const VirtualTour = () => {
     return (
@@ -13,6 +14,7 @@ const VirtualTour = () => {
           <iframe width="100%" height="640" frameborder="0" allow="xr-spatial-tracking; gyroscope; accelerometer" allowfullscreen scrolling="no" src="https://kuula.co/share/collection/7FWFX?logo=1&info=1&fs=1&vr=0&sd=1&thumbs=1"></iframe>
           </div>
         </div>
+        <Footer />
         </>
     );
 };
