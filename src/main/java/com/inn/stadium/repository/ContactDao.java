a6027e309429416dba82ccd8b6eba1c774a5681f
RigonP <rp57421@ubt-uni.net>
2023-09-08 20:07:17 +0200

    Shtimi i metodave per marrjen e gjithe kontakteve, perditesimin e kontaktit dhe marrja sipas ID, ne interface

--- src/main/java/com/inn/stadium/repository/ContactDao.java
+++ src/main/java/com/inn/stadium/repository/ContactDao.java
@@ -1,4 +1,26 @@
 package com.inn.stadium.repository;
 
-public interface ContactDao {
+import com.inn.stadium.POJO.Contact;
+import com.inn.stadium.wrapper.ContactWrapper;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+public interface ContactDao extends JpaRepository<Contact, Integer> {
+
+    // Metoda për të marrë të gjitha kontaktet në një listë të objekteve ContactWrapper.
+    List<ContactWrapper>getAllContact();
+
+    // Metoda për të përditësuar statusin e një kontakti me anë të një ID-je të caktuar.
+    @Transactional
+    @Modifying
+    Integer updateContactStatus(@Param("status") String status, @Param("id") Integer id);
+
+    // Metoda për të marrë një kontakt duke përdorur një ID të caktuar dhe kthyer si objekt ContactWrapper.
+    ContactWrapper getContactById(@Param("id") Integer id);
+
+
 }
