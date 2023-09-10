282ec8c4dd5e643e9e494ba562ef37d3293a2489
RigonP <rp57421@ubt-uni.net>
2023-09-10 16:10:33 +0200

    Repozitori i Spring Data JPA, dhe percaktimi i metodave.

--- src/main/java/com/inn/stadium/repository/REventsDao.java
+++ src/main/java/com/inn/stadium/repository/REventsDao.java
@@ -1,4 +1,23 @@
 package com.inn.stadium.repository;
 
-public interface REventsDao {
+import com.inn.stadium.POJO.REvents;
+import com.inn.stadium.wrapper.REventsWrapper;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+
+public interface REventsDao  extends JpaRepository<REvents,Integer> {
+
+    List<REventsWrapper> getAllEvents();
+    @Modifying
+    @Transactional
+    Integer updateEventStatus(@Param("status") String status, @Param("id") Integer id);
+
+    List<REventsWrapper>getEventsByCategory(@Param("id") Integer id);
+
+
 }
