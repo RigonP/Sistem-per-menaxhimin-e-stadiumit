d4ac8452733562fdc81ef9dfb5bcdb8f490ea71e
RigonP <rp57421@ubt-uni.net>
2023-09-10 20:27:37 +0200

    Komentimi i konstruktoreve

--- src/main/java/com/inn/stadium/wrapper/REventsWrapper.java
+++ src/main/java/com/inn/stadium/wrapper/REventsWrapper.java
@@ -19,6 +19,7 @@ public class REventsWrapper {
 
     }
 
+    //Konstruktori per metoden getAllEvents
     public REventsWrapper(Integer i, String t, String l, String d,String o, String p, String s, String cN, Integer cId){
         id = i;
         titulli = t;
@@ -31,11 +32,15 @@ public class REventsWrapper {
         categoryId = cId;
     }
 
+
+    //Konstruktori per metoden getEventByCategory
     public REventsWrapper(Integer i, String t){
         id = i;
         titulli = t;
     }
 
+
+    //Konstruktori per metoden getEventById
     public REventsWrapper(Integer i, String t, String l, String d, String o, String p){
         id = i;
         titulli = t;
