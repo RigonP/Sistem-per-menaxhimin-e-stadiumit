629de4c79ef0a8b30f2127e2d94a5aa41f9a788b
RigonP <rp57421@ubt-uni.net>
2023-09-10 20:22:45 +0200

    Krijimi i konstruktoreve per metodat qe kthejne rezultat

--- src/main/java/com/inn/stadium/wrapper/REventsWrapper.java
+++ src/main/java/com/inn/stadium/wrapper/REventsWrapper.java
@@ -1,4 +1,47 @@
 package com.inn.stadium.wrapper;
 
+import lombok.Data;
+
+@Data
 public class REventsWrapper {
+
+    Integer id;
+    String titulli;
+    String lokacioni;
+    String data;
+    String ora;
+    String pershkrimi;
+    String statusi;
+    String categoryName;
+    Integer categoryId;
+
+    public REventsWrapper(){
+
+    }
+
+    public REventsWrapper(Integer i, String t, String l, String d,String o, String p, String s, String cN, Integer cId){
+        id = i;
+        titulli = t;
+        lokacioni = l;
+        data = d;
+        ora = o;
+        pershkrimi = p;
+        statusi = s;
+        categoryName = cN;
+        categoryId = cId;
+    }
+
+    public REventsWrapper(Integer i, String t){
+        id = i;
+        titulli = t;
+    }
+
+    public REventsWrapper(Integer i, String t, String l, String d, String o, String p){
+        id = i;
+        titulli = t;
+        lokacioni = l;
+        data = d;
+        ora = o;
+        pershkrimi = p;
+    }
 }
