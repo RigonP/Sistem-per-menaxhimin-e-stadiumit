a45f75a671bcb3867fa35430c0aecb5937c6a801
RigonP <rp57421@ubt-uni.net>
2023-09-10 17:56:10 +0200

    Komentim i kodit

--- src/main/java/com/inn/stadium/service/REventsService.java
+++ src/main/java/com/inn/stadium/service/REventsService.java
@@ -1,4 +1,27 @@
 package com.inn.stadium.service;
 
+import com.inn.stadium.wrapper.REventsWrapper;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Map;
+
+
+//Kjo eshte nje nderfaqe (interface) qe perdoret per menaxhimin e eventeve ne stadium.
 public interface REventsService {
+
+    // Metoda per shtimin e nje eventi te ri
+    ResponseEntity<String> addNewEvent(Map<String, String> requestMap);
+
+    ResponseEntity<List<REventsWrapper>> getAllEvents();
+
+    ResponseEntity<String> updateEvents(Map<String, String> requestMap);
+
+    ResponseEntity<String> deleteEvent(Integer id);
+
+    ResponseEntity<String> updateStatus(Map<String, String> requestMap);
+
+    ResponseEntity<List<REventsWrapper>> getByCategory(Integer id);
+
+    ResponseEntity<REventsWrapper> getEventById(Integer id);
 }
