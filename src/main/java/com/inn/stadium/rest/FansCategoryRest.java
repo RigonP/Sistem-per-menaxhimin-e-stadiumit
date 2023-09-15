da6e89a3cec10cb10a0932adfcefdbf198449a5a
RigonP <rp57421@ubt-uni.net>
2023-09-15 16:42:49 +0200

    Implementimi i metodave add, get, update

--- src/main/java/com/inn/stadium/rest/FansCategoryRest.java
+++ src/main/java/com/inn/stadium/rest/FansCategoryRest.java
@@ -1,4 +1,22 @@
 package com.inn.stadium.rest;
 
+
+import com.inn.stadium.POJO.FansCategory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RequestMapping(path = "/fansCategory")
 public interface FansCategoryRest {
+
+    @PostMapping(path = "/add")
+    public ResponseEntity<String> addNewFansCategory(@RequestBody(required = true)Map<String, String> requestMap);
+
+    @GetMapping(path = "/get")
+    public ResponseEntity<List<FansCategory>> getAllFansCategory(@RequestParam(required = false) String filterValue);
+
+    @PostMapping(path = "/update")
+    public ResponseEntity<String> updateFansCategory (@RequestBody(required = true) Map<String, String> requestMap);
 }
--- src/main/java/com/inn/stadium/restImpl/FansCategoryRestImpl.java
+++ src/main/java/com/inn/stadium/restImpl/FansCategoryRestImpl.java
@@ -1,6 +1,25 @@
 package com.inn.stadium.restImpl;
 
+import com.inn.stadium.POJO.FansCategory;
 import com.inn.stadium.rest.FansCategoryRest;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Map;
 
 public class FansCategoryRestImpl implements FansCategoryRest {
+    @Override
+    public ResponseEntity<String> addNewFansCategory(Map<String, String> requestMap) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<FansCategory>> getAllFansCategory(String filterValue) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<String> updateFansCategory(Map<String, String> requestMap) {
+        return null;
+    }
 }
