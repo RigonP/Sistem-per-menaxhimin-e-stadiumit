e43529626c5b6d8b753d2de8d23686df2f9aa6d1
RigonP <rp57421@ubt-uni.net>
2023-09-17 00:11:07 +0200

    Kontrolli per akses te paautorizuar dhe blloku try-catch

--- src/main/java/com/inn/stadium/service/AdministrataService.java
+++ src/main/java/com/inn/stadium/service/AdministrataService.java
@@ -1,4 +1,19 @@
 package com.inn.stadium.service;
 
+import com.inn.stadium.wrapper.AdministrataWrapper;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Map;
+
 public interface AdministrataService {
+    ResponseEntity<String> addNewAdministrata(Map<String, String> requestMap);
+
+    ResponseEntity<List<AdministrataWrapper>> getAllAdministrata();
+
+    ResponseEntity<String> updateAdministrata(Map<String, String> requestMap);
+
+    ResponseEntity<String> deleteAdministrata(Integer id);
+
+    ResponseEntity<AdministrataWrapper> getAdministrataById(Integer id);
 }
