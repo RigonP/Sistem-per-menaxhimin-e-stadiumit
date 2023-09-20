4942dfc98141db09d0ec91cd6ebccce6f6f28caa
RigonP <rp57421@ubt-uni.net>
2023-09-20 20:09:17 +0200

    Identifikimi i path kryesor dhe paths per metodat, implementimi i tyre ne klase

---
+++ src/main/java/com/inn/stadium/rest/TourRest.java
@@ -0,0 +1,29 @@
+package com.inn.stadium.rest;
+
+
+import com.inn.stadium.wrapper.TourWrapper;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RequestMapping(path = "/tour")
+public interface TourRest {
+
+    @PostMapping(path = "/add")
+    public ResponseEntity<String> addNewTour(@RequestBody Map<String, String> requestMap);
+
+    @GetMapping(path = "/get")
+    public ResponseEntity<List<TourWrapper>> getAllTour();
+
+    @PostMapping(path = "/update")
+    public ResponseEntity<String> updateTour(@RequestBody Map<String, String> requestMap);
+
+    @PostMapping(path = "/delete/{id}")
+    public ResponseEntity<String> deleteTour(@PathVariable Integer id);
+
+    @GetMapping(path = "/getById")
+    public ResponseEntity<TourWrapper> getById(@PathVariable Integer id);
+}
---
+++ src/main/java/com/inn/stadium/serviceImpl/TourServiceImpl.java
@@ -0,0 +1,6 @@
+package com.inn.stadium.serviceImpl;
+
+import com.inn.stadium.service.TourService;
+
+public class TourServiceImpl implements TourService {
+}
